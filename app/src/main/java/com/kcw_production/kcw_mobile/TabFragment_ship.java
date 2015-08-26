8ba7f0096e582f8381c5afb4454ac09bba9f4bf1
jushe <792988035@qq.com>
2015-08-26 23:53:36 +0800

    TabFragment_ship 1.Rewrite TabFragment_ship.java 2.Add new string into String.xml 3.Adjust the layout of ship

--- app/src/main/java/com/kcw_production/kcw_mobile/TabFragment_ship.java
+++ app/src/main/java/com/kcw_production/kcw_mobile/TabFragment_ship.java
@@ -5,6 +5,8 @@ package com.kcw_production.kcw_mobile;
  */
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +15,22 @@ import android.view.ViewGroup;
 /**
  * Created by airfr on 2015/8/24.
  */
-public class TabFragment_ship extends Fragment {
+
+public  class TabFragment_ship extends Fragment{
+    private RecyclerView shipRecyclerView;
+    //private LinearLayoutManager layoutManager;
+    private String[] shipName = {"长门", "长门改","陆奥", "陆奥改"};
+    private ShipAdapter shipAdapter;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
-        return inflater.inflate(R.layout.tab_fragment_ship,container,false);
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup container,Bundle saveInstanceBundle){
+        View v = layoutInflater.inflate(R.layout.tab_fragment_ship, container, false);
+        shipAdapter = new ShipAdapter(getActivity());
+        shipAdapter.iniData(shipName);
+        shipRecyclerView = (RecyclerView) v.findViewById(R.id.ship_map_list);
+        shipRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        shipRecyclerView.setAdapter(shipAdapter);
+        return v;
     }
+
 }
