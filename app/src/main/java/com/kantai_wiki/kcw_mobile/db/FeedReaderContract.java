cb1656abca5353a6fba8adc29d12358f46863ed4
Wafer-Li <omyshokami@gmail.com>
2015-08-29 00:43:07 +0900

    Complete the contract class

--- app/src/main/java/com/kantai_wiki/kcw_mobile/db/FeedReaderContract.java
+++ app/src/main/java/com/kantai_wiki/kcw_mobile/db/FeedReaderContract.java
@@ -2,6 +2,7 @@ package com.kantai_wiki.kcw_mobile.db;
 
 import android.provider.BaseColumns;
 
+
 /**
  * Created by Wafer on 2015/8/28.
  *
@@ -65,14 +66,139 @@ public final class FeedReaderContract {
         public static final String COLUMN_NOTICE = "notice";
     }
 
-    // TODO: 2015/8/28 Add Equipment_kms
-    // TODO: 2015/8/28 Add Equipment_enemy
-    // TODO: 2015/8/28 Add Equipment_upgrade
+
+    //Equipment_kms
+    private static abstract class EquipmentKMS implements BaseColumns {
+
+        //Design of the table
+        public static final String TABLE_NAME = "EquipmentKMS";
+        public static final String COLUMN_ID = "id";
+        public static final String COLUMN_RARE_RATE = "rare_rate";
+        public static final String COLUMN_CATEGORY = "category";
+        public static final String COLUMN_VALUE_FIREPOWER = "value_firepower";
+        public static final String COLUMN_VALUE_TORPEDO = "value_torpedo";
+        public static final String COLUMN_VALUE_EXPLOSION = "value_explosion";
+        public static final String COLUMN_VALUE_ANTIAIRCRAFT = "value_antiaircraft";
+        public static final String COLUMN_VALUE_ANTISUBMARINE = "value_antisubmarine";
+        public static final String COLUMN_VALUE_TRACKING = "value_tracking";
+        public static final String COLUMN_VALUE_ACCURACY = "value_accuracy";
+        public static final String COLUMN_VALUE_EVASION = "value_evasion";
+        public static final String COLUMN_RANGE = "range";
+        public static final String COLUMN_EQUIPPABLE = "equippable";
+        public static final String COLUMN_NOTICE = "notice";
+    }
+
+
+    //Equipment_enemy
+    private static abstract class EquipmentEnemy implements BaseColumns {
+
+        //Design of the table
+        public static final String TABLE_NAME = "EquipmentEnemy";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_CATEGORY = "category";
+        public static final String COLUMN_RANGE = "range";
+        public static final String COLUMN_VALUE_FIREPOWER = "value_firepower";
+        public static final String COLUMN_VALUE_TORPEDO = "value_torpedo";
+        public static final String COLUMN_VALUE_ANTIAIRCRAFT = "value_antiaircraft";
+        public static final String COLUMN_VALUE_ACCURACY = "value_accuracy";
+        public static final String COLUMN_VALUE_TRACKING = "value_tracking";
+        public static final String COLUMN_VALUE_EXPLOSION = "value_explosion";
+        public static final String COLUMN_VALUE_ANTISUBMARINE = "value_antisubmarine";
+        public static final String COLUMN_VALUE_EVASION = "value_evasion";
+        public static final String COLUMN_VALUE_ARMOR = "value_armor";
+    }
+
+    //Equipment_upgrade
+    private static abstract class EquipmentUpgrade implements BaseColumns {
+
+        //Design of the table
+        public static final String TABLE_NAME = "EquipmentUpgrade";
+        public static final String COLUMN_CATEGORY = "category";
+        public static final String COLUMN_POSITION = "position";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_STAR = "star";
+        public static final String COLUMN_CONSUME_FUEL = "consume_fuel";
+        public static final String COLUMN_CONSUME_BULLET = "consume_bullet";
+        public static final String COLUMN_CONSUME_STEEL = "consume_steel";
+        public static final String COLUMN_CONSUME_ALUM = "consume_alum";
+        public static final String COLUMN_CONSUME_MATERIAL = "consume_material";
+        public static final String COLUMN_CONSUME_SCREW = "consume_screw";
+        public static final String COLUMN_CONSUME_EQUIPMENT = "consume_equipment";
+        public static final String COLUMN_UPGRADEABLE_DAY = "upgradeable_day";
+        public static final String COLUMN_ASSISTANT_SHIP = "assistant_ship";
+        public static final String COLUMN_UPGRADE_RESULT = "upgrade_result";
+        public static final String COLUMN_UPGRADE_RESULT_INVENTABLE = "upgrade_result_inventeable";
+    }
 
     //SQL Command
 
+    //Equipment_upgrade
+    private static final String SQL_CREAT_EQUIPMENTUPGRADE = "create table " + EquipmentUpgrade.TABLE_NAME + " (" +
+            EquipmentUpgrade._ID + " integer primary key autoincrement" + COMMA_SET +
+            EquipmentUpgrade.COLUMN_CATEGORY + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_POSITION + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_NAME + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_STAR + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_CONSUME_FUEL + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_CONSUME_BULLET + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_CONSUME_STEEL + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_CONSUME_ALUM + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_CONSUME_MATERIAL + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_CONSUME_SCREW + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_CONSUME_EQUIPMENT + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_UPGRADEABLE_DAY + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_ASSISTANT_SHIP + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_UPGRADE_RESULT + TYPE_TEXT + COMMA_SET +
+            EquipmentUpgrade.COLUMN_UPGRADE_RESULT_INVENTABLE + TYPE_TEXT + COMMA_SET +
+            " )";
+
+    private static final String SQL_DELETE_EQUIPMENTUPGRADE =
+            "drop table if exists " + EquipmentUpgrade.TABLE_NAME;
+
+    //Equipment_enemy
+    private static final String SQL_CREAT_EQUIPMENTENEMY = "create table " + EquipmentEnemy.TABLE_NAME + " (" +
+            EquipmentEnemy._ID + " integer primary key autoincrement" + COMMA_SET +
+            EquipmentEnemy.COLUMN_NAME + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_CATEGORY + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_RANGE + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_VALUE_FIREPOWER + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_VALUE_TORPEDO + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_VALUE_ANTIAIRCRAFT + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_VALUE_ACCURACY + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_VALUE_TRACKING + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_VALUE_EXPLOSION + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_VALUE_ANTISUBMARINE + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_VALUE_EVASION + TYPE_TEXT + COMMA_SET +
+            EquipmentEnemy.COLUMN_VALUE_ARMOR + TYPE_TEXT + COMMA_SET +
+            " )";
+
+    private static final String SQL_DELETE_EQUIPMENTENEMY =
+            "drop table if exists " + EquipmentEnemy.TABLE_NAME;
+
+    //EquipmentKMS
+    private static final String SQL_CREAT_EQUIPMENTKMS = "create table " + EquipmentKMS.TABLE_NAME + " (" +
+            EquipmentKMS._ID + " integer primary key autoincrement" + COMMA_SET +
+            EquipmentKMS.COLUMN_ID + TYPE_INTEGER + COMMA_SET +
+            EquipmentKMS.COLUMN_RARE_RATE + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_CATEGORY + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_VALUE_FIREPOWER + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_VALUE_TORPEDO + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_VALUE_EXPLOSION + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_VALUE_ANTIAIRCRAFT + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_VALUE_ANTISUBMARINE + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_VALUE_TRACKING + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_VALUE_ACCURACY + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_VALUE_EVASION + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_RANGE + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_EQUIPPABLE + TYPE_TEXT + COMMA_SET +
+            EquipmentKMS.COLUMN_NOTICE + TYPE_TEXT + COMMA_SET +
+            " )";
+
+    private static final String SQL_DELETE_EQUIPMENTKMS =
+            "drop table if exists " + EquipmentKMS.TABLE_NAME;
+
     //Quest
-    public static final String SQL_CREAT_QUEST = "create table " + Quest.TABLE_NAME + " (" +
+    private static final String SQL_CREAT_QUEST = "create table " + Quest.TABLE_NAME + " (" +
             Quest._ID + " integer primary key autoincrement" + COMMA_SET +
             Quest.COLUMN_CATEGORY + TYPE_TEXT + COMMA_SET +
             Quest.COLUMN_TYPE + TYPE_TEXT + COMMA_SET +
@@ -85,14 +211,14 @@ public final class FeedReaderContract {
             Quest.COLUMN_REWARD_ALUM + TYPE_TEXT + COMMA_SET +
             Quest.COLUMN_REWARD_OTHER + TYPE_TEXT + COMMA_SET +
             Quest.COLUMN_CONDITION + TYPE_TEXT + COMMA_SET +
-            Quest.COLUMN_NOTICE + TYPE_TEXT + COMMA_SET +
+            Quest.COLUMN_NOTICE + TYPE_TEXT +
             " )";
 
-    public static final String SQL_DELETE_QUEST =
+    private static final String SQL_DELETE_QUEST =
             "drop table if exists " + Quest.TABLE_NAME;
 
     //Expedition
-    public static final String SQL_CREAT_EXPEDITION = "create table " + Expedition.TABLE_NAME + " (" +
+    private static final String SQL_CREAT_EXPEDITION = "create table " + Expedition.TABLE_NAME + " (" +
             Expedition._ID + " integer primary key autoincrement" + COMMA_SET +
             Expedition.COLUMN_ID + TYPE_INTEGER + COMMA_SET +
             Expedition.COLUMN_NAME + TYPE_TEXT + COMMA_SET +
@@ -114,7 +240,7 @@ public final class FeedReaderContract {
             Expedition.COLUMN_NOTICE + TYPE_TEXT +
             " )";
 
-    public static final String SQL_DELETE_EXPEDITION =
+    private static final String SQL_DELETE_EXPEDITION =
             "drop table if exists " + Expedition.TABLE_NAME;
 
 }
