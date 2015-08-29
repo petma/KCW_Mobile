3b7d43edf0d09b43f1b22af8d692ad29e2951994
jushe <792988035@qq.com>
2015-08-29 10:34:14 +0800

    ShipInformationActivity 1.add ShipInfromationActivity 2.add ship_information.xml

---
+++ app/src/main/java/com/kantai_wiki/kcw_mobile/ShipInformationActivity.java
@@ -0,0 +1,20 @@
+package com.kantai_wiki.kcw_mobile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 2015/8/29.
+ */
+public class ShipInformationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ship_information);
+        /** show the ships' information
+         *  through the position by Intent to judge which ship it is
+         *  Intent intent = getIntent()
+         *  ...
+         */
+    }
+}
