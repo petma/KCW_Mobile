37b59ed1b4fc0369a524f8b6d7614017ba3b2ea5
Graham Tang <airfree1452@gmail.com>
2015-08-31 23:23:53 +0800

    Add Navigation

--- app/src/main/java/com/kantai_wiki/kcw_mobile/MainActivity.java
+++ app/src/main/java/com/kantai_wiki/kcw_mobile/MainActivity.java
@@ -1,12 +1,22 @@
 package com.kantai_wiki.kcw_mobile;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SwitchDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        mynavi();
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText(this.getResources().getString(R.string.TabFragment_1)));
         tabLayout.addTab(tabLayout.newTab().setText(this.getResources().getString(R.string.TabFragment_2)));
@@ -47,7 +57,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void mynavi(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withHeader(R.layout.navi_head)
+                .withHeaderDivider(false)
+                .withActionBarDrawerToggle(true)
+                .withTranslucentStatusBarShadow(false)
+                .withTranslucentStatusBar(false)
+                .withHeaderDivider(false)
+                .withStatusBarColorRes(R.color.crepusculo_kantai_main_primarydark)
+                .withSliderBackgroundColorRes(R.color.md_white_1000)
+                .addDrawerItems(
 
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_back).withIdentifier(1).withIcon(R.drawable.ic_back),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_settings).withIdentifier(2).withIcon(R.drawable.ic_settings),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_update).withIdentifier(3).withIcon(R.drawable.ic_loop),
+                        new DividerDrawerItem().withTag("艦隊"),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_help).withIdentifier(4).withIcon(R.drawable.ic_help_outline),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_about).withIdentifier(5).withIcon(R.drawable.ic_favorite_border),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_exit).withIdentifier(6).withIcon(R.drawable.ic_exit),
+                        new DividerDrawerItem().withTag("Wiki"),
+                        new SwitchDrawerItem().withName(R.string.drawer_item_nightmode).withIdentifier(7)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        int id = drawerItem.getIdentifier();
+                        switch (id){
+                            case 1:
+
+                                return true;
+                            case 2:
+                                closeOptionsMenu();
+                                return true;
+                            case 3:
+                            case 4:
+                            case 5:
+                                Snackbar.make(view, "You Click "+id+"th button", Snackbar.LENGTH_LONG).show();
+                                return true;
+                            case 6:
+                                finish();
+                                return true;
+                            case 7:
+                                return true;
+                        }return true;
+                    }
+                })
+                .build();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -63,10 +125,13 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings1) {
-            return true;
-        }
+        switch (id){
+            case 1:
 
+            case 2:
+
+                return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
