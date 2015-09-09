da7de99b00dce6d03bee684816475bafaada3a6d
jushe <792988035@qq.com>
2015-09-09 17:52:23 +0800

    add the shipnames

--- app/src/main/java/com/kantai_wiki/kcw_mobile/activity/TabFragment_ship.java
+++ app/src/main/java/com/kantai_wiki/kcw_mobile/activity/TabFragment_ship.java
@@ -24,21 +24,46 @@ import java.util.List;
  */
 
 public  class TabFragment_ship extends Fragment {
-    private RecyclerView shipRecyclerView;
     private static final int CLOSE = 0;
     private static final int OPEN = 1;
-    //private LinearLayoutManager layoutManager;
-    private String[] shipClass1 = {"驱逐舰", "轻巡/雷巡", "重巡/航巡", "战舰", "正规空母", "潜水舰", "其它舰艇"};
-    private String[] shipClass2 = {"返回上一级", "次级分类", "次级分类", "次级分类", "次级分类"};
-    private String[] shipClass3 = {"返回上一级", "三级分类", "三级分类", "三级分类", "三级分类"};
+    /**
+     *  Here are the ship class and their name
+     */
+    private String[] shipType = {"驱逐舰", "轻巡/雷巡", "重巡/航巡", "战舰", "正规空母", "轻母/水母", "潜水舰", "其它舰艇"};
+    private String[] DD = {"睦月", "如月", "弥生", "卯月", "皋月", "文月", "长月", "菊月", "三日月", "望月"
+            ,"吹雪", "白雪", "初雪", "深雪", "从云", "矶波", "绫波", "敷波"
+            ,"胧", "曙", "涟", "潮", "晓", "响", "雷", "电","初春", "子日", "若叶", "初霜"
+            ,"白露", "时雨", "村雨", "夕立", "春雨", "五月雨", "海风", "江风", "凉风"
+            ,"朝潮", "大潮", "满潮", "荒潮"
+            ,"朝云", "山云", "霰", "霞", "阳炎", "不知火", "黑潮", "初风", "雪风", "天津风", "时津风", "浦风", "叽风"
+            ,"滨风", "谷风", "野分", "秋云", "秋月", "照月", "岛风", "Z1", "Z3", "利伯齐奥"
+            , "夕云", "卷云", "风云", "长波", "高波", "朝霜", "早霜", "清霜"};
+    private String[] CL = {"天龙", "龙田", "球磨", "多摩", "北上", "大井", "木曾", "长良", "五十铃"
+            ,"名取", "由良", "鬼怒", "阿武隈", "川内", "神通", "那珂", "夕张"
+            ,"阿贺野", "能代", "矢矧", "酒匂", "大淀" };
+    private String[] CA = {"古鹰", "加古", "青叶", "衣笠", "妙高", "那智", "足柄", "羽黑", "高雄"
+            ,"爱宕", "摩耶", "鸟海", "最上", "三隈", "铃谷", "熊野", "利根", "筑摩"
+            ,"欧根亲王"};
+    private String[] BB = {"金刚", "比叡", "榛名", "雾岛", "扶桑", "山城"
+            ,"伊势", "日向", "长门", "陆奥", "大和", "武藏"
+            ,"俾斯麦", "维托里奥", "罗马"};
+    private String[] CV = {"赤城", "加贺", "苍龙", "飞龙", "翔鹤", "瑞鹤"
+            ,"云龙", "天成", "葛城", "大凤"};
+    private String[] SS = {"伊168", "伊8", "伊19", "伊58", "吕500", "丸输", "U-511", "伊401"};
+    private String[] CVL ={"瑞穗", "秋津洲", "凤翔", "龙骧", "龙凤"
+            ,"翔凤", "瑞凤", "飞鹰", "隼鹰", "千岁", "千代田"};
+    private String[] OS = {"大鲸"};
+    /******************* ships' name end here ************************/
+
     private int[] classStatus = {0,0,0,0,0,0,0};
     private ShipAdapter shipAdapter;
+    private RecyclerView shipRecyclerView;
 
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle saveInstanceBundle) {
         View v = layoutInflater.inflate(R.layout.tab_fragment_ship, container, false);
         shipAdapter = new ShipAdapter(getActivity());
-        shipAdapter.iniData(shipClass1);
+        shipAdapter.iniData(shipType);
         shipRecyclerView = (RecyclerView) v.findViewById(R.id.ship_map_list);
         shipRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         shipRecyclerView.setAdapter(shipAdapter);
@@ -57,84 +82,96 @@ public  class TabFragment_ship extends Fragment {
         switch(shipAdapter.getShipData().get(position)){
             case"驱逐舰": {
                 if (classStatus[0] != OPEN) {
-                    shipAdapter.addAllItem(shipClass2,position);
+                    shipAdapter.addAllItem(DD,position);
                     //shipAdapter.addAllItem(shipClass2, position);
                     classStatus[0] = OPEN;
                 }
                 else{
-                    shipAdapter.removeAllItem(shipClass2);
+                    shipAdapter.removeAllItem(DD);
                     classStatus[0] = CLOSE;
                 }
                 break;
             }
             case "轻巡/雷巡":{
                 if (classStatus[1] != OPEN) {
-                    shipAdapter.addAllItem(shipClass2, position);
+                    shipAdapter.addAllItem(CL, position);
                     //shipAdapter.addAllItem(shipClass2, position);
                     classStatus[1] = OPEN;
                 }
                 else{
-                    shipAdapter.removeAllItem(shipClass2);
+                    shipAdapter.removeAllItem(CL);
                     classStatus[1] = CLOSE;
                 }
                 break;
             }
             case "重巡/航巡":{
                 if (classStatus[2] != OPEN) {
-                    shipAdapter.addAllItem(shipClass2,position);
-                   // shipAdapter.addAllItem(shipClass2, position);
+                    shipAdapter.addAllItem(CA,position);
+                    // shipAdapter.addAllItem(shipClass2, position);
                     classStatus[2] = OPEN;
                 }
                 else{
-                    shipAdapter.removeAllItem(shipClass2);
+                    shipAdapter.removeAllItem(CA);
                     classStatus[2] = CLOSE;
                 }
                 break;
             }
             case "战舰":{
                 if (classStatus[3] != OPEN) {
-                    shipAdapter.addAllItem(shipClass2,position);
-                   // shipAdapter.addAllItem(shipClass2, position);
+                    shipAdapter.addAllItem(BB,position);
+                    // shipAdapter.addAllItem(shipClass2, position);
                     classStatus[3] = OPEN;
                 }
                 else{
-                    shipAdapter.removeAllItem(shipClass2);
+                    shipAdapter.removeAllItem(BB);
                     classStatus[3] = CLOSE;
                 }
                 break;
             }
             case "正规空母":{
                 if (classStatus[4] != OPEN) {
-                    shipAdapter.addAllItem(shipClass2,position);
-                   // shipAdapter.addAllItem(shipClass2, position);
+                    shipAdapter.addAllItem(CV,position);
+                    // shipAdapter.addAllItem(shipClass2, position);
                     classStatus[4] = OPEN;
                 }
                 else{
-                    shipAdapter.removeAllItem(shipClass2);
+                    shipAdapter.removeAllItem(CV);
                     classStatus[4] = CLOSE;
+                }
+                break;
+            }
+            case "轻母/水母":{
+                if (classStatus[5] != OPEN) {
+                    shipAdapter.addAllItem(CVL,position);
+                    // shipAdapter.addAllItem(shipClass2, position);
+                    classStatus[5] = OPEN;
+                }
+                else{
+                    shipAdapter.removeAllItem(CVL);
+                    classStatus[5] = CLOSE;
                 }
                 break;
             }
             case "潜水舰":{
                 if (classStatus[5] != OPEN) {
-                    shipAdapter.addAllItem(shipClass2,position);
-                   // shipAdapter.addAllItem(shipClass2, position);
+                    shipAdapter.addAllItem(SS,position);
+                    // shipAdapter.addAllItem(shipClass2, position);
                     classStatus[5] = OPEN;
                 }
                 else{
-                    shipAdapter.removeAllItem(shipClass2);
+                    shipAdapter.removeAllItem(SS);
                     classStatus[5] = CLOSE;
                 }
                 break;
             }
             case "其它舰艇":{
                 if (classStatus[6] != OPEN) {
-                    shipAdapter.addAllItem(shipClass2,position);
-                   // shipAdapter.addAllItem(shipClass2, position);
+                    shipAdapter.addAllItem(OS,position);
+                    // shipAdapter.addAllItem(shipClass2, position);
                     classStatus[6] = OPEN;
                 }
                 else{
-                    shipAdapter.removeAllItem(shipClass2);
+                    shipAdapter.removeAllItem(OS);
                     classStatus[6] = CLOSE;
                 }
                 break;
@@ -144,5 +181,8 @@ public  class TabFragment_ship extends Fragment {
             }
         }
     }
+
+
+
 
 }
