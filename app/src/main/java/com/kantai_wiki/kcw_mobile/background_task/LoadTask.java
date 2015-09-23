6d3b93dd804b57dc05e5055ae7070fa6b5a470c8
Wafer-Li <omyshokami@gmail.com>
2015-09-23 21:38:58 +0900

    Adjust the back-end logic

--- app/src/main/java/com/kantai_wiki/kcw_mobile/background_task/LoadTask.java
+++ app/src/main/java/com/kantai_wiki/kcw_mobile/background_task/LoadTask.java
@@ -4,18 +4,26 @@ import android.content.Context;
 import android.os.AsyncTask;
 
 import com.kantai_wiki.kcw_mobile.db.KCWDB;
+import com.kantai_wiki.kcw_mobile.model.EquipmentEnemy;
+import com.kantai_wiki.kcw_mobile.model.EquipmentKMS;
+import com.kantai_wiki.kcw_mobile.model.EquipmentUpgrade;
+import com.kantai_wiki.kcw_mobile.model.Expedition;
+import com.kantai_wiki.kcw_mobile.model.Quest;
+import com.kantai_wiki.kcw_mobile.util.HTTPUtils;
+import com.kantai_wiki.kcw_mobile.util.Parser;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Created by Wafer on 2015/8/31.
  *
  */
-public class LoadTask extends AsyncTask<Integer, Void, List<?>> {
+public class LoadTask extends AsyncTask<Void, Void, List<?>> {
 
-    public TaskResponse taskResponse;
-    public LoadResult loadResult;
+    public TaskListener taskListener;
     private Context context;
+    private int taskTypes;
 
     /**
      * The LoadTask extends AsyncTask
@@ -26,28 +34,118 @@ public class LoadTask extends AsyncTask<Integer, Void, List<?>> {
      * @param context The current context which you are running
      */
 
-    public LoadTask(Context context) {
+    public LoadTask(Context context,int taskType,TaskListener taskListener) {
         this.context = context;
+        this.taskListener = taskListener;
+        this.taskTypes = taskType;
     }
 
     @Override
-    protected List<?> doInBackground(Integer... taskTypes) {
+    protected List<?> doInBackground(Void... param) {
         KCWDB kcwdb = KCWDB.getInstance(context);
 
-        switch (taskTypes[0]) {
+        switch (taskTypes) {
 
             case TaskTypeContract.TASK_TYPE_QUEST:
-                return kcwdb.loadQuest();
+                List<Quest> questList;
+                questList = kcwdb.loadQuest();
+                if (questList.isEmpty()) {
+
+                    //sync
+                    try {
+                        String response;
+                        response = HTTPUtils.download(HTTPUtils.getURL(taskTypes));
+                        questList = Parser.praseQuest(response);
+                        kcwdb.syncQuest(questList);
+                    } catch (IOException e) {
+                        return null;
+                    } catch (IllegalAccessException e) {
+                        return null;
+                    }
+                }
+
+                return questList;
+
             case TaskTypeContract.TASK_TYPE_EXPEDITION:
-                return kcwdb.loadExpedition();
+                List<Expedition> expeditionList;
+                expeditionList = kcwdb.loadExpedition();
+                if (expeditionList.isEmpty()) {
+
+                    //sync
+                    try {
+                        String response;
+                        response = HTTPUtils.download(HTTPUtils.getURL(taskTypes));
+                        expeditionList = Parser.praseExpedition(response);
+                        kcwdb.syncExpedition(expeditionList);
+                    } catch (IOException e) {
+                        return null;
+                    } catch (IllegalAccessException e) {
+                        return null;
+                    }
+                }
+
+                return expeditionList;
+
             case TaskTypeContract.TASK_TYPE_EQUIPMENT_KMS:
-                return kcwdb.loadEquipmentKMS();
+                List<EquipmentKMS> equipmentKMSList;
+                equipmentKMSList = kcwdb.loadEquipmentKMS();
+                if (equipmentKMSList.isEmpty()) {
+
+                    //sync
+                    try {
+                        String response;
+                        response = HTTPUtils.download(HTTPUtils.getURL(taskTypes));
+                        equipmentKMSList = Parser.parseEquipmentKMS(response);
+                        kcwdb.syncEquipmentKMS(equipmentKMSList);
+                    } catch (IOException e) {
+                        return null;
+                    } catch (IllegalAccessException e) {
+                        return null;
+                    }
+                }
+
+                return equipmentKMSList;
+
             case TaskTypeContract.TASK_TYPE_EQUIPMENT_ENEMY:
-                return kcwdb.loadEquipmentEnemy();
+                List<EquipmentEnemy> equipmentEnemyList;
+                equipmentEnemyList = kcwdb.loadEquipmentEnemy();
+                if (equipmentEnemyList.isEmpty()) {
+
+                    //sync
+                    String response;
+                    try {
+                        response = HTTPUtils.download(HTTPUtils.getURL(taskTypes));
+                        equipmentEnemyList = Parser.parseEquipmentEnemy(response);
+                        kcwdb.syncEquipmentEnemy(equipmentEnemyList);
+                    } catch (IOException e) {
+                        return null;
+                    } catch (IllegalAccessException e) {
+                        return null;
+                    }
+                }
+
+                return equipmentEnemyList;
             case TaskTypeContract.TASK_TYPE_EQUIPMENT_UPGRADE:
-                return kcwdb.loadEquipmentUpgrade();
+                List<EquipmentUpgrade> equipmentUpgradeList;
+                equipmentUpgradeList = kcwdb.loadEquipmentUpgrade();
+                if (equipmentUpgradeList.isEmpty()) {
+
+                    //sync
+                    String response;
+                    try {
+                        response = HTTPUtils.download(HTTPUtils.getURL(taskTypes));
+                        equipmentUpgradeList = Parser.parseEquipmentUpgrade(response);
+                        kcwdb.syncEquipmentUpgrade(equipmentUpgradeList);
+                    } catch (IOException e) {
+                        return  null;
+                    } catch (IllegalAccessException e) {
+                        return null;
+                    }
+                }
+                return equipmentUpgradeList;
+
             default:
-                taskResponse.processFinish(false, "The task type is illegal");
+                break;
         }
 
         return null;
@@ -55,6 +153,6 @@ public class LoadTask extends AsyncTask<Integer, Void, List<?>> {
 
     @Override
     protected void onPostExecute(List<?> objects) {
-        loadResult.getLoadResult(objects);
+        taskListener.onResult(objects);
     }
 }
---
+++ app/src/main/java/com/kantai_wiki/kcw_mobile/background_task/TaskListener.java
@@ -0,0 +1,23 @@
+package com.kantai_wiki.kcw_mobile.background_task;
+
+import java.util.List;
+
+/**
+ * Created by Wafer on 2015/8/31.
+ *
+ */
+
+
+public interface TaskListener {
+
+    /**
+     * This method is a callback method,
+     * you should implement this method if you want to get the result.
+     * @param list This is the List you sync/load for,
+     *             please cast it to which you want,
+     *             care for the risk of force cast.
+     * @return I set the return type as a List,
+     *         you could just return the list you receive.
+     */
+    List<?> onResult(List<?> list);
+}
